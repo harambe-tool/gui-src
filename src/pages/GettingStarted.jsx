205fb3034240b7a46efb94ddf35fc0df35780724
Bedros <pastrikk00@gmail.com>
2024-05-29 01:14:39 +0400

    Added glow for dragging to indicate that yes, drag and drop is supported

--- src/pages/GettingStarted.jsx
+++ src/pages/GettingStarted.jsx
@@ -6,6 +6,7 @@ export default function Importer() {
   const ctx = useContext(AppStateContext);
   const navigate = useNavigate();
   const [file, setFile] = useState(null);
+  const [dragging, setDragging] = useState(false);
 
   const handleFileChange = (event) => {
     setFile(event.target.files[0]);
@@ -27,28 +28,46 @@ export default function Importer() {
     }
   };
 
+  function preventAndStopPropagation(e){
+      e.preventDefault();
+      e.stopPropagation();
+  }
+  function dragHandle(e, value) {
+    preventAndStopPropagation(e)
+    setDragging(value);
+  }
+
   return (
     <>
-      <div onDragOver={e=>{e.preventDefault();e.stopPropagation();}} style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100vh" }}>
-        <div id="banner" style={{ display: "flex", alignItems: "center", marginBottom: "20px" }}>
-          <span style={{ fontSize: "2em" }}>WhoDunnit</span>
-          <span>Find links between requests and fuzz with AI.</span>
+    <div onDragEnd={e=>{dragHandle(e,false)}} onDragOver={e=>{dragHandle(e,true)}} onDragLeave={e=>{dragHandle(e,false)}} style={{height:"100vh"}}>
+      
+        <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+          <div id="banner" style={{ display: "flex", alignItems: "center", marginBottom: "20px" }}>
+            <span style={{ fontSize: "2em" }}>WhoDunnit</span>
+            <span>Find links between requests and fuzz with AI.</span>
+          </div>
+          <div className="sep"></div>
+          <div>
+            <p>Insert your HAR file here</p>
+            <label htmlFor="file-upload" style={{ display: "flex", flexDirection:"column", cursor: "pointer" }}>
+              <input ref={inputRef} accept=".har" multiple={false} type="file" id="file-upload" onChange={handleFileChange} style={{ display: "none" }} />
+              <button 
+                onDrop={(drag)=>{
+                  dragHandle(drag,false);
+                  drag.dataTransfer.files[0] && setFile(drag.dataTransfer.files[0])
+                }} 
+                onClick={()=>{inputRef.current.click()}} 
+                style={{ borderRadius: "5px", padding: "10px", border: "1px solid #ccc", background:"rgba(255, 255, 255, 0.05)", cursor: "pointer", boxShadow:dragging?"0px -1px 20px 3px #867cff":"none" }}>
+                Choose File
+              </button>
+              <span style={{ display:"block" }}>{file?.name ?? "No file selected"}</span>
+            </label>
+          </div>
+            { file && <button onClick={()=>handleUpload()} style={{ borderRadius: "5px", padding: "10px", border: "1px solid rgba(255, 255, 255, 0.2)", margin:"50px", background:"rgba(255, 255, 255, 0.05)", cursor: "pointer"}}>
+              Get Started
+            </button>}
         </div>
-        <div className="sep"></div>
-        <div>
-          <p>Insert your HAR file here</p>
-          <label htmlFor="file-upload" style={{ display: "flex", flexDirection:"column", cursor: "pointer" }}>
-            <input ref={inputRef} accept=".har" multiple={false} type="file" id="file-upload" onChange={handleFileChange} style={{ display: "none" }} />
-            <button onDrop={(drag)=>{console.log(drag);drag.preventDefault(); drag.stopPropagation();drag.dataTransfer.files[0] && setFile(drag.dataTransfer.files[0])}} onClick={()=>{inputRef.current.click()}} style={{ borderRadius: "5px", padding: "10px", border: "1px solid #ccc", background:"rgba(255, 255, 255, 0.05)", cursor: "pointer" }}>
-              Choose File
-            </button>
-            <span style={{ display:"block" }}>{file?.name ?? "No file selected"}</span>
-          </label>
-        </div>
-          { file && <button onClick={()=>handleUpload()} style={{ borderRadius: "5px", padding: "10px", border: "1px solid rgba(255, 255, 255, 0.2)", margin:"50px", background:"rgba(255, 255, 255, 0.05)", cursor: "pointer"}}>
-            Get Started
-          </button>}
-      </div>
+    </div>
     </>
   );
 }
